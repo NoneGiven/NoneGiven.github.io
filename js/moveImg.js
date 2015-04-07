3efd7d611506ddfe6c19c610c879ceb59c871d7b
NoneGiven <NoneGiven@users.noreply.github.com>
2015-04-06 20:33:32 -0500

    Update moveImg.js

--- js/moveImg.js
+++ js/moveImg.js
@@ -1,5 +1,6 @@
 (function() {
-  var theImg = null;
+  var theImg, uInt, dInt, lInt, rInt
+  theImg = null;
   function shiftImg(dir, amt) {
     var pos;
     if (dir == "lr") {
@@ -19,16 +20,31 @@
       return;
     }
     if (e.keyCode == 65) { // A
-      shiftImg("lr", -1);
+      lInt = window.setInterval(function() { shiftImg("lr", -1); }, 500);
     }
     else if (e.keyCode == 68) { // D
-      shiftImg("lr", 1);
+      rInt = window.setInterval(function() { shiftImg("lr", 1); }, 500);
     }
     else if (e.keyCode == 87) { // W
-      shiftImg("ud", -1);
+      uInt = window.setInterval(function() { shiftImg("ud", -1); }, 500);
     }
     else if (e.keyCode == 83) { // S
-      shiftImg("ud", 1);
+      dInt = window.setInterval(function() { shiftImg("ud", 1); }, 500);
+    }
+  }
+  
+  function keyUpCheck(e) {
+    if (e.keyCode == 65) { // A
+      window.clearInterval(lInt);
+    }
+    else if (e.keyCode == 68) { // D
+      window.clearInterval(rInt);
+    }
+    else if (e.keyCode == 87) { // W
+      window.clearInterval(uInt);
+    }
+    else if (e.keyCode == 83) { // S
+      window.clearInterval(dInt);
     }
   }
   
@@ -52,5 +68,6 @@
     window.setTimeout(grabImgLoop, 10);
   }
   document.addEventListener("keydown", keyCheck);
+  document.addEventListener("keyup", keyUpCheck);
   
 }).call();
