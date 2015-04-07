c4032b0c6c634cc77ebd5bbcb37d93a91cd26c58
NoneGiven <NoneGiven@users.noreply.github.com>
2015-04-06 20:18:19 -0500

    Update moveImg.js

--- js/moveImg.js
+++ js/moveImg.js
@@ -1,21 +1,37 @@
 (function() {
-  function shiftImage(amt) {
-    var pos = parseInt(theImg.style.left);
-    pos += amt;
-    theImg.style.left = pos;
+  function shiftImage(dir, amt) {
+    var pos;
+    if (dir == "lr") {
+       pos = parseInt(theImg.style.left);
+       pos += amt * 5;
+      theImg.style.left = pos;
+    }
+    else {
+      pos = parseInt(theImg.style.top);
+      pos += amt * 5;
+      theImg.style.top = pos;
+    }
   }
   
   function keyCheck(e) {
     if (e.keyCode == 37) {
-      shiftImg(-1);
+      shiftImg("lr", -1);
     }
     else if (e.keyCode == 39) {
-      shiftImg(1);
+      shiftImg("lr", 1);
     }
+    else if (e.keyCode == 38) {
+      shiftImg("ud", -1);
+    }
+    else if (e.keyCode == 40) {
+      shiftImg("ud", 1);
+    }
+  }
     
-    var theImg = document.getElementById("shiftyImg");
-    if (theImg) {
-      document.addEventListener("keydown", keyCheck);
-    }
+  var theImg = document.getElementById("shiftyImg");
+  if (theImg) {
+    theImg.style.left = "0px";
+    theImg.style.top = "0px";
+    document.addEventListener("keydown", keyCheck);
   }
 }).call();
