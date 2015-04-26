960c0a5c881d60030f95ba8355f088008a783840
NoneGiven <NoneGiven@users.noreply.github.com>
2015-04-25 23:24:35 -0500

    Update keycode.js

--- js/keycode.js
+++ js/keycode.js
@@ -8,10 +8,40 @@
     altSpan = document.getElementById("altkey");
     metaSpan = document.getElementById("metakey");
     document.addEventListener("keydown", keyCheck);
+    document.addEventListener("keyup", keyUpCheck);
   }
   
   function keyCheck(e) {
-    displayKey(e.keyCode);
+    if (e.keyCode != 16 && e.keyCode != 17 && e.keyCode != 18) {
+      displayKey(e.keyCode);
+    }
+    if (e.shiftKey) {
+      modifier(shiftSpan, true);
+    }
+    if (e.ctrlKey) {
+      modifier(ctrlSpan, true);
+    }
+    if (e.altKey) {
+      modifier(altSpan, true);
+    }
+    if (e.metaKey) {
+      modifier(metaSpan, true);
+    }
+  }
+  
+  function keyUpCheck(e) {
+    if (e.shiftKey) {
+      modifier(shiftSpan, false);
+    }
+    if (e.ctrlKey) {
+      modifier(ctrlSpan, false);
+    }
+    if (e.altKey) {
+      modifier(altSpan, false);
+    }
+    if (e.metaKey) {
+      modifier(metaSpan, false);
+    }
   }
   
   function displayKey(code) {
@@ -19,7 +49,12 @@
   }
   
   function modifier(elem, on) {
-    
+    if (on) {
+      elem.className = "";
+    }
+    else {
+      elem.className = "off";
+    }
   }
   
   document.addEventListener("DOMContentLoaded", startup);
