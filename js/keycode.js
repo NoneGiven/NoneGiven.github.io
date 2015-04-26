039a3eb23c02f4b292b4e56060645de0ab05fdbf
NoneGiven <NoneGiven@users.noreply.github.com>
2015-04-25 23:18:31 -0500

    Update keycode.js

--- js/keycode.js
+++ js/keycode.js
@@ -1,3 +1,26 @@
 (function() {
-
+  var mainSpan, shiftSpan, ctrlSpan, altSpan, metaSpan;
+  
+  function startup() {
+    mainSpan = document.getElementById("keydisplay");
+    shiftSpan = document.getElementById("shiftkey");
+    ctrlSpan = document.getElementById("ctrlkey");
+    altSpan = document.getElementById("altkey");
+    metaSpan = document.getElementById("metakey");
+    document.addEventListener("keydown", keyCheck);
+  }
+  
+  function keyCheck(e) {
+    displayKey(e.keyCode);
+  }
+  
+  function displayKey(code) {
+    mainSpan.innerHTML = code;
+  }
+  
+  function modifier(elem, on) {
+    
+  }
+  
+  document.addEventListener("DOMContentLoaded", startup);
 }).call();
