6666396b566c8318afbbf2558ea41e076372de39
NoneGiven <NoneGiven@users.noreply.github.com>
2015-04-25 23:28:31 -0500

    Update keycode.js

--- js/keycode.js
+++ js/keycode.js
@@ -15,33 +15,18 @@
     if (e.keyCode != 16 && e.keyCode != 17 && e.keyCode != 18) {
       displayKey(e.keyCode);
     }
-    if (e.shiftKey) {
-      modifier(shiftSpan, true);
-    }
-    if (e.ctrlKey) {
-      modifier(ctrlSpan, true);
-    }
-    if (e.altKey) {
-      modifier(altSpan, true);
-    }
-    if (e.metaKey) {
-      modifier(metaSpan, true);
-    }
+    modifier(shiftSpan, e.shiftKey);
+    modifier(ctrlSpan, e.ctrlKey);
+    modifier(altSpan, e.altKey);
+    modifier(metaSpan, e.metaKey);
+    
   }
   
   function keyUpCheck(e) {
-    if (e.shiftKey) {
-      modifier(shiftSpan, false);
-    }
-    if (e.ctrlKey) {
-      modifier(ctrlSpan, false);
-    }
-    if (e.altKey) {
-      modifier(altSpan, false);
-    }
-    if (e.metaKey) {
-      modifier(metaSpan, false);
-    }
+    modifier(shiftSpan, e.shiftKey);
+    modifier(ctrlSpan, e.ctrlKey);
+    modifier(altSpan, e.altKey);
+    modifier(metaSpan, e.metaKey);
   }
   
   function displayKey(code) {
