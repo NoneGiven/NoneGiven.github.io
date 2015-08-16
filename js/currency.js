8f32301bbdb4addd7c8edec55a1ea9e2b8b2ab0a
NoneGiven <NoneGiven@users.noreply.github.com>
2015-08-16 11:37:21 -0400

    Update currency.js

--- js/currency.js
+++ js/currency.js
@@ -1,3 +1,28 @@
 (function() {
-
+  var conversions = null;
+  function setup() {
+    document.removeEventListener("DOMContentLoaded", setup);
+    clone();
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.fixer.io/latest");
+    xhr.onload = (function() { x = this.response });
+    xhr.send();
+  }
+  function clone() {
+    document.getElementById("to").innerHTML = document.getElementById("from").innerHTML;
+  }
+  function convert(amount, from, to) {
+    if (conversions === null) {
+      return;
+    }
+    if (from in conversions.rates && to in conversions.rates) {
+      return amount / conversions.rates[from] * conversions.rates[to];
+    }
+  }
+  function submit() {
+    if (conversions === null) {
+      return;
+    }
+  }
+  document.addEventListener("DOMContentLoaded", setup);
 }).call();
