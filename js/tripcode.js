89eeb853c7c91f49fb111a7ae271eead390bed17
NoneGiven <NoneGiven@users.noreply.github.com>
2015-11-04 17:05:46 -0600

    Update tripcode.js

--- js/tripcode.js
+++ js/tripcode.js
@@ -1,4 +1,18 @@
 "use strict";
 (function() {
-
+  console.log("script start: " + performance.now());
+  var sjisReady = false;
+  
+  function addListeners() {
+    document.removeEventListener("DOMContentLoaded", addListeners);
+  }
+  
+  function setReady() {
+    document.removeEventListener("sjisconvDone", setReady);
+    console.log("sjis ready: " + performance.now());
+    sjisReady = true;
+  }
+  
+  document.addEventListener("sjisconvDone", setReady);
+  document.addEventListener("DOMContentLoaded", addListeners);
 }).call(this);
