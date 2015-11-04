7128409d1f33b1f5bcda2f8b409a9e7b09cee30c
NoneGiven <NoneGiven@users.noreply.github.com>
2015-11-04 17:24:42 -0600

    Update tripcode.js

--- js/tripcode.js
+++ js/tripcode.js
@@ -1,18 +1,61 @@
 "use strict";
 (function() {
-  console.log("script start: " + performance.now());
-  var sjisReady = false;
+  var readyCount = 0;
+  var tickInterval = 100;
+  var tickIntervalContainer = null;
+  var currentPass = "";
+  var passInput = null;
+  var tripInput = null;
   
-  function addListeners() {
+  function sjisEncode(pass) {
+    var conv = "";
+    var char = null;
+    for (var i = 0; i < pass.length; i++) {
+      char = window.sjisconv[pass];
+      if (char === undefined) {
+        char = "?";
+      }
+      conv += char;
+    }
+    return conv;
+  }
+  
+  function makeTripcode(pass) {
+    return sjisEncode(pass);
+  }
+  
+  function listenTick() {
+    currentPass = passInput.value;
+    if (!currentPass) {
+      tripInput.value = "";
+      return;
+    }
+    if (currentPass.length > 8) {
+      currentPass = currentPass.substr(0, 8);
+    }
+    tripInput.value = "!" + makeTripcode(currentPass;)
+  }
+  
+  function startListening() {
+    passInput = document.getElementById("pass");
+    tripInput = document.getElementById("trip");
+    tickIntervalContainer = setInterval(listenTick, tickInterval);
+  }
+  
+  function onDOMContentLoaded() {
     document.removeEventListener("DOMContentLoaded", addListeners);
+    if (++readyCount > 1) {
+      startListening();
+    }
   }
   
-  function setReady() {
+  function onSjisconvDone() {
     document.removeEventListener("sjisconvDone", setReady);
-    console.log("sjis ready: " + performance.now());
-    sjisReady = true;
+    if (++readyCount > 1) {
+      startListening();
+    }
   }
   
-  document.addEventListener("sjisconvDone", setReady);
-  document.addEventListener("DOMContentLoaded", addListeners);
+  document.addEventListener("sjisconvDone", onSjisconvDone);
+  document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
 }).call(this);
