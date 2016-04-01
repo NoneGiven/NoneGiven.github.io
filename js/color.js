5e74a39066fdda8871a099b7988e6a426a08e43b
NoneGiven <NoneGiven@users.noreply.github.com>
2016-04-01 13:43:59 -0400

    Update color.js

--- js/color.js
+++ js/color.js
@@ -1,1 +1,27 @@
-// hi
+(function() {
+  var e = {
+    "color-box": null,
+    "rgb-r": null,
+    "rgb-g": null,
+    "rgb-b": null,
+    "hex": null,
+    "int-r": null,
+    "int-g": null,
+    "int-b": null,
+    "int-r": null,
+    "int-g": null,
+    "int-b": null,
+    "hsv-h": null,
+    "hsv-s": null,
+    "hsv-v": null
+  };
+  
+  function setup() {
+    for (let elem in e) {
+      e[elem] = document.getElementById(elem);
+      e[elem].removeAttribute("disabled");
+    }
+  }
+  
+  document.addEventListener("DOMContentLoaded", setup);
+}).call(this);
