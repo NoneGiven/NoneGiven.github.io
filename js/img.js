48c268bded221509df04ced11732885630182361
NoneGiven <NoneGiven@users.noreply.github.com>
2016-07-31 20:27:27 -0400

    Update img.js

--- js/img.js
+++ js/img.js
@@ -124,7 +124,7 @@
     sequences = [];
     seqIndex = 0;
     var elems = byClass("sequence");
-    for (let i = elements.length - 1; i >= 0; i--) {
+    for (let i = elems.length - 1; i >= 0; i--) {
       let elem = elems[i];
       let seq = { };
       let type = elem.byClass("type")[0].selectedIndex;
